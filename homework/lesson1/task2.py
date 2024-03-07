2c81c651f2f463975dea752e0ab388dd898637a0
Tanya <tanya@example.com>
2024-03-07 15:15:18 +0300

    added task2

---
+++ homework/lesson1/task2.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route()
+def main():
+    context = {'title': ''}
+    return render_template()
+
+@app.route()
+def main():
+    context = {'title': ''}
+    return render_template()
+
+@app.route()
+def main():
+    context = {'title': ''}
+    return render_template()
+
